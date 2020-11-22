8cd1232828bf6c71994358c10e6b39d96b3e17ff
Avinash Sharma <ravinashkvafs@gmail.com>
2020-11-22 17:42:46 +0530

    build added + heroku live

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import * as express from 'express';
+import { join } from 'path';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors();
+
+  // app.use(express.static(join(process.cwd(), './public')));
+  app.use('/', express.static(join(__dirname, '../public'), { etag: false }));
+
+  // console.log(join(process.cwd(), './public'))
+
+  await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
